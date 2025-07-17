db91bef289b3271c297e0050c3ba40145f952966
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2025-07-17 21:03:33 +0600

    Update auth_provider.dart

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,44 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-
-// class AuthProvider with ChangeNotifier {
-//   bool _isLoggedIn = false;
-
-//   bool get isLoggedIn => _isLoggedIn;
-
-//   Future<void> checkLoginStatus() async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     _isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
-//     notifyListeners();
-//   }
-
-//   Future<void> login() async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     await prefs.setBool('isLoggedIn', true);
-//     _isLoggedIn = true;
-//     notifyListeners();
-//   }
-
-//   Future<void> logout() async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     await prefs.clear();
-//     _isLoggedIn = false;
-//     notifyListeners();
-//   }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -57,7 +16,6 @@ class AuthProvider with ChangeNotifier {
   }
 
   Future<void> login(String email, String password) async {
-    // You can add validation logic here or API call
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setBool('isLoggedIn', true);
     await prefs.setString('userEmail', email);
